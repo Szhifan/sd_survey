8896f8618aa309854a60a2aa1e0eb727faae5781
Zhifan Sun <sun@slurm-login.ukp.informatik.tu-darmstadt.de>
2024-09-05 11:50:04 +0200

    mg

--- survey.py
+++ survey.py
@@ -188,22 +188,11 @@ class SDSurvey:
                 self.last_page()
             else:
                 self.annotation_page(self.pages.current)
-    
-def test():
-    uri = "mongodb+srv://zhifan:dudu8802616@cluster0.nau3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-            # Create a new client and connect to the server
-    client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+ 
 
 if __name__ == "__main__":
-    test()
-    # st.set_page_config("Stance Detection Annotation",layout="wide")
-    # sv = SDSurvey()
-    # sv.run_app()
+    
+    st.set_page_config("Stance Detection Annotation",layout="wide")
+    sv = SDSurvey()
+    sv.run_app()
     
